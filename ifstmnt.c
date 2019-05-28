b455c45e10c0b47751d2dc6ca728a7f030493b3a
tua1415 <50212123+tua1415@users.noreply.github.com>
2019-05-27 18:12:37 -0700

    Create ifstmnt.c

---
+++ ifstmnt.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main()
+{
+  int a = 0;
+  if (a == 0)
+  {
+	printf("a is 0\n");
+  }
+  else
+  {
+	printf("a is not 0\n");
+  }
+
+  if (a != 0)
+  {
+	printf("a is not 0\n");
+  }
+  else
+  {
+	printf("a is 0\n");
+  }
+
+  if (a > 0)
+  {
+	printf("a is greater than 0\n");
+  }
+  else
+  {
+	printf("a is not greater than 0\n");
+  }
+
+  if (a >= 0)
+  {
+	printf("a is not 0\n");
+  }
+  else
+  {
+	printf("a is 0\n");
+  }
+
+  if (a > 0)
+  {
+	printf("a is greater than 0\n");
+  }
+  else
+  {
+	printf("a is not greater than 0\n");
+  }
+
+  if (a >= 0)
+  {
+	printf("a is greater than or equal to 0\n");
+  }
+  else
+  {
+ 	 printf("a is not greater than or equal to 0\n");
+  }
+}
