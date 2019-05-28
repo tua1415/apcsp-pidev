1932557d07f2dcff1bf8f19de402e794df5b2dc9
tua1415 <50212123+tua1415@users.noreply.github.com>
2019-05-28 10:18:28 -0700

    commit

--- sizeofvariables.c
+++ sizeofvariables.c
@@ -8,7 +8,6 @@ int main()
   double d = 123.456;
   unsigned int e = 7;
   signed int f = -10;
-
   printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
   printf("float b value: %f and size: %d bytes\n", b, sizeof(b));
   printf("char c value: %c and size: %d bytes\n", c, sizeof(c));
